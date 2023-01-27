3742b490100eaf9af5cb4d916edafd78087abf0f
jangeltr <jangeltr@hotmail.com>
2023-01-27 08:19:17 +0000

    verison corregida

--- mongo.js
+++ mongo.js
@@ -1,35 +1,29 @@
-const {MongoClient, ObjectId} = require('mongodb')
+const {MongoClient} = require('mongodb')
 
-const DB_HOST = 'localhost'
-const DB_PORT = '27017'
 const DB_NAME = 'users'
-
-const MONGO_URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}?retryWrites=true&w=majority`
+const MONGO_URL = 'mongodb://127.0.0.1:27017?maxPoolSize=20&w=majority'
 
 class Mongo{
     constructor(){
-        this.client = new MongoClient(MONGO_URI,{useNewUrlParser : true})
+        this.client = new MongoClient(MONGO_URL)
+        this.db = null
     }
-    connect(){
-        if (!Mongo.connection){
-            Mongo.connection = new Promise((resolve, reject)=>{
-                this.client.connect(err => {
-                    if (err) {
-                        console.log('NO se conecto a la BD')
-                        reject(err)
-                    }
-                    console.log('Se conecto a la BD')
-                    resolve(this.client.db())
-                })
-            })
+    async connect(){
+        await this.client.connect()
+        console.log('SE REALIZO UNA CONEXION A LA BD')
+        this.db=this.client.db(DB_NAME)
+    }
+    disconnect(){
+        this.client.close()
+    }
+    async getAll(collection, query){
+        console.log('Recibio una peticion getAll a '+collection)
+        let findResult
+        if (!this.db){
+            await this.connect()
         }
-        return Mongo.connection
-    }
-    getAll(collection, query){
-        console.log('getAll:'+collection)
-        return this.connect().then(db=>{
-            return db.collection(collection).find(query).toArray()
-        })
+        findResult = await this.db.collection(collection).find(query).toArray()
+        return findResult
     }
     getOne(collection, query){
 
--- routes.js
+++ routes.js
@@ -7,11 +7,27 @@ function escucha(app){
     const misUsuarios = new Usuarios()
 
     router.get('/data', async function(req, res, next){
-        console.log('recibio peticion get en /api/datos')
+        console.log('recibio peticion get en /api/datos ')
+        console.log('parametros: ',req.params)
+        console.log('query',req.query)//http://localhost:9090/api/data?nombre=angel&edad=26
         try{
-            const users = misUsuarios.getUsuarios({})
+            const users = await misUsuarios.getUsuarios(req.query)
             res.status(200).json({
                 'usuarios':users            
+            })
+        }catch(error){
+            console(error)
+        }
+    })
+    router.post('/data', async function(req, res, next){
+        console.log('recibio peticion post en /api/datos ')
+        console.log('parametros: ',req.params)
+        console.log('body: ',req.body)
+        console.log('query',req.query)
+        try{
+            const users = await misUsuarios.getUsuarios({})
+            res.status(200).json({
+                'Peticion post ':'Satisfactoria'            
             })
         }catch(error){
             console(error)
